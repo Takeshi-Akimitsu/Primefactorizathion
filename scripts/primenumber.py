d33a3493e2dc87a72e88c38878252460183747f6
Takeshi Akimitsu <takeshiakimitsu@example.com>
2022-01-27 19:21:14 +0000

    Add a file

--- scripts/primenumber.py
+++ scripts/primenumber.py
@@ -9,18 +9,33 @@ rate = rospy.Rate(10)
 while not rospy.is_shutdown():
     print('Prease input a number:',end = ' ')
     n = int(input())
-
     prime_list = []
-    for i in range(2,n+1):
-        prime_list.append(i)
-        for p in range(2,i):
-            if i % p == 0:
-                prime_list.remove(i)
-                break
-    prime_list.append(n)
-    
-    prime = Int32MultiArray(data=prime_list)
 
+    if n == 0:
+        print("0 is not primenumber")
+        prime_list = [n]
+    
+    elif n == 1:
+        print("1 is not primenumber")
+        prime_list = [n]
+
+    else:
+        for i in range(2,n+1):
+            prime_list.append(i)
+            for j in range(2,i):
+                if i % j == 0:
+                    prime_list.remove(i)
+                    break
+        
+        if n == prime_list[len(prime_list)-1]:
+            print('{0} is primenumber'.format(n))
+            
+    prime_list.append(n)
+
+    prime = Int32MultiArray(data=prime_list)
+    
+    print(prime_list)
+    
     pub.publish(prime)
     rate.sleep()
 
